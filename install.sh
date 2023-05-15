98a01ded821128114b260b9c9f5cae6edf21c880
Norman Koch <norman.koch@tu-dresden.de>
2023-05-15 15:43:14 +0200

    initial commit

--- install.sh
+++ install.sh
@@ -10,5 +10,7 @@ source ~/.reminder_environment/bin/activate
 pip install timefhuman easygui holidays matplotlib pandas tabulate
 
 mkdir -p ~/.config/systemd/user/
-cat reminder.service | sed -e "s#{basepath}#${PWD}#" > ~/.config/systemd/user/something.service
+cat reminder.service | sed -e "s#{basepath}#${PWD}#" > ~/.config/systemd/user/reminder.service
 systemctl --user daemon-reload
+
+systemctl --user enable reminder
