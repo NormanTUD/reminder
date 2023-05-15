d3a749a84f5c3be31cce65a82ef43c73975f13d5
Norman Koch <norman.koch@tu-dresden.de>
2023-05-15 15:26:50 +0200

    initial commit

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ python3 -m venv ~/.reminder_environment
 source ~/.reminder_environment/bin/activate
 
 pip install timefhuman easygui holidays matplotlib pandas tabulate
+
+mkdir -p ~/.config/systemd/user/
+cat reminder.service | sed -e "s#{basepath}#${PWD}#" > ~/.config/systemd/user/something.service
